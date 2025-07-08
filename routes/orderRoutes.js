4e2683d6911da7f7d4dfb65033b4f55e6d99b882
Amrutha-3175 <23r11a0543@gcet.edu.in>
2025-07-08 14:26:01 +0530

    u

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,8 +1,23 @@
-import mongoose from 'mongoose'
-const orderSchema = mongoose.Schema({
-  email: { type: String },
-  orderValue: { type: Number },
-});
-// const product = mongoose.model("Product", productSchema);
+import express from "express";
+import orderModel from "./models/orderModel.js";
 
-export default mongoose.model("Order", orderSchema);
+const orderRouter = express.Router();
+
+orderRouter.post("/new", async (req, res) => {
+  const { email, orderValue } = req.body;
+  const result = await orderModel.create({ email, orderValue });
+  return res.json(result);
+});
+
+orderRouter.get("/:id", async (req, res) => {
+  const email = req.params.id;
+  const result = await orderModel.find({ email });
+  return res.json(result);
+});
+
+orderRouter.get("/all", async (req, res) => {
+  const result = await orderModel.find();
+  return res.json(result);
+});
+
+export default orderRouter;
